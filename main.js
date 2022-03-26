6ae7698481a3273970943eb4bd012ca0607fd20a
Eddie Quintana <Eduardo.Quintana@ameritas.com>
2022-03-26 15:43:12 -0600

    Added some comments

--- main.js
+++ main.js
@@ -2,8 +2,10 @@ function checkForMatchingBrackets() {
     let string = document.getElementById("myText").value;
     let pattern = /^(\{(?:[^}{]|\{(?:[^}{]|\{[^}{]*\})*\})*\})/g;
     let braces = 0;
-
+    
+    // Looping through each string character
     for(var i = 0; i < string.length; i++) {
+        //if index matches an opening or closing curly brace, increment braces count
         if(i === "{" || i === "}")
             ++braces
     }
